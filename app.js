f806fe55d14b8a041c82470b594e8620a1d681d7
AgentSMDJ <sandermae036@gmail.com>
2026-09-08 13:04:26 +0200

    added the script to find the things

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const place = document.querySelector("div");
 console.log(place);
 
-fetch("https://icanhazdadjoke.com").then(function (response){
-    console.log(response)
+fetch("https://icanhazdadjoke.com", {
+    headers: {
+        "accept": "application/json"
+    }
+}
+
+
+).then(function (response) {
+    return response.json();
+}).then(function (data) {
+    place.textContent = data.joke
 })
